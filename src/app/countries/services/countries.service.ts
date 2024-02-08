928ae3e5e9184ea9702669f646767f7596fa36c8
alexDev2808 <colores.2808.8000@gmail.com>
2024-02-07 19:53:11 -0600

    Refactorizando countries services

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, delay, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -9,6 +9,14 @@ export class CountriesService {
     private apiURL: string = 'https://restcountries.com/v3.1';
 
     constructor( private httpClient: HttpClient ) { }
+
+    private getCountriesRequest( url : string ) : Observable<Country[]> {
+        return this.httpClient.get<Country[]>( url )
+            .pipe(
+                catchError( () => of([])),
+                delay( 2000 ),
+            )
+    }
 
     searchCountryByAlphaCode( code: string ) : Observable<Country | null> {
         const url = `${ this.apiURL }/alpha/${ code }`;
@@ -23,28 +31,19 @@ export class CountriesService {
     searchCapital( term: string ) : Observable<Country[]> {
         const url = `${ this.apiURL }/capital/${ term }`;
         
-        return this.httpClient.get<Country[]>( url )
-            .pipe(
-                catchError( error => of([]))
-            )
+        return this.getCountriesRequest( url );
     }
 
     searchByCountry( term : string ) : Observable<Country[]> {
         const url = `${ this.apiURL }/name/${ term }`
 
-        return this.httpClient.get<Country[]>( url )
-            .pipe(
-                catchError( error => of([]))
-            )
+        return this.getCountriesRequest( url );
     }
 
     searchByRegion( region : string ) : Observable<Country[]> {
         const url = `${ this.apiURL }/region/${ region }`
 
-        return this.httpClient.get<Country[]>( url )
-            .pipe(
-                catchError( error => of([]))
-            )
+        return this.getCountriesRequest( url );
     }
     
 }
