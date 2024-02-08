c5d800fe2695a25d6077e274bebb3b2d8e0ba9a7
alexDev2808 <colores.2808.8000@gmail.com>
2024-02-07 21:01:22 -0600

    buscar por region: Optimizacion

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas'| 'Asia'| 'Europe'| 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -11,13 +13,21 @@ import { CountriesService } from '../../services/countries.service';
 export class ByRegionPageComponent {
 
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
+  public isLoading = false;
 
   constructor( private countriesService: CountriesService ) { }
 
-  searchByRegion( region : string ) {
+  searchByRegion( region : Region ) {
+
+    this.selectedRegion = region;
+    this.isLoading = true;
+
     this.countriesService.searchByRegion( region )
       .subscribe(countries => {
         this.countries = countries
+        this.isLoading = false;
       })
   }
 
